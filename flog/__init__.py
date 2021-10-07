d42ebc68faaeb71f73d10a0e42375e27479aeb17
Matthew Halse <matthew.halse@hey.com>
2021-10-07 15:15:45 -0500

    Use Poetry for package and library management
    * Add a pyproject.toml to replace setup.py, requirements.txt, and
      manifest.in
    * Move version and description from __init__.py to pyproject.toml.
    * Remove tox, Travis CI definition, Makefile, etc.
    * Use Python unittest's builtin runner in GitHub Actions.
    * Generate a more detailed git ignore file

--- flog/__init__.py
+++ flog/__init__.py
@@ -1,6 +1,3 @@
 from .flog import CorrelationLoggerAdapter, get_logger, log_call, log_sensitive_call
 
-__version__ = "0.3.0"
-__description__ = "Fast access to some simple python logging tricks"
-
 __all__ = ["CorrelationLoggerAdapter", "get_logger", "log_call", "log_sensitive_call"]
